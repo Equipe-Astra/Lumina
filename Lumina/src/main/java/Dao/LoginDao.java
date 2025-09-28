56d2a470df0aabaa9b10d322eb73ccda9fdebceb
Marcela Stade <78057087+marcelastade@users.noreply.github.com>
2025-09-28 13:30:49 -0300

    Voltando ao  LoginDAO antigo

--- Lumina/src/main/java/Dao/LoginDao.java
+++ Lumina/src/main/java/Dao/LoginDao.java
@@ -6,11 +6,14 @@ import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
-import Dto.FuncionarioInfo;
-
 public class LoginDao {
 	private EntityManagerFactory emf;
 	private EntityManager em;
+
+	public LoginDao() {
+		emf = Persistence.createEntityManagerFactory("Lumina");
+		em = emf.createEntityManager();
+	}
 
 	public String buscaUsuario(String email) {
 		try {
@@ -43,34 +46,18 @@ public class LoginDao {
 	}
 
 	public String buscaCargo(String usuario) {
-		try {
-			String sql = "SELECT b.descricao FROM funcionarios a JOIN cargo b ON a.id_cargo = b.id_cargo WHERE lower(a.id_funcionario) = lower(:usuario)";
-			Query query = em.createNativeQuery(sql);
-			query.setParameter("usuario", usuario);
+			try {
+				String sql = "SELECT b.descricao FROM funcionarios a JOIN cargo b ON a.id_cargo = b.id_cargo WHERE lower(a.id_funcionario) = lower(:usuario)";
+				Query query = em.createNativeQuery(sql);
+				query.setParameter("usuario", usuario);
 
-			if (query.getSingleResult() == null) {
+				if (query.getSingleResult() == null) {
+					return null;
+				}
+				return query.getSingleResult().toString();
+			} catch (NoResultException e) {
 				return null;
 			}
-			return query.getSingleResult().toString();
-		} catch (NoResultException e) {
-			return null;
 		}
-	}
 
-	public FuncionarioInfo buscaUsuarioCompleto(String email) {
-		EntityManager em = JpaUtil.getEntityManager();
-		try {
-			String sql = "SELECT f.id_funcionario, f.senha, c.descricao " + "FROM funcionarios f "
-					+ "JOIN cargo c ON f.id_cargo = c.id_cargo " + "WHERE lower(f.email) = lower(:email)";
-			Query query = em.createNativeQuery(sql);
-			query.setParameter("email", email);
-
-			Object[] resultado = (Object[]) query.getSingleResult();
-			return new FuncionarioInfo(resultado[0].toString(), resultado[1].toString(), resultado[2].toString());
-		} catch (NoResultException e) {
-			return null;
-		} finally {
-			em.close(); 
-		}
-	}
 }
